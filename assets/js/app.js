92c133590330ca3f40e89ce1b835ec127d73fa08
Valentin <valentin.grenier02@gmail.com>
2023-05-23 11:58:55 +0200

    Ajout animations + cursor custom

---
+++ assets/js/app.js
@@ -0,0 +1,38 @@
+const app =
+{
+  init: function()
+  {
+    const button = document.querySelector('#scrolltotop');
+    button.addEventListener('click', app.handleClick);
+
+    document.addEventListener('scroll', app.handleScroll);
+  },
+
+  handleClick: function()
+  {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  },
+
+  handleScroll: function()
+  {
+    const button = document.querySelector('#scrolltotop');
+
+    if(window.scrollY > 250)
+    {
+      button.classList.add('show');
+    }
+    else
+    {
+      button.classList.remove('show');
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', app.init);
+
+
+
+
+
+
+
