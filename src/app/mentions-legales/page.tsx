02aa36d53871b05a817fc42f7e9f21ba9fa44965
Valentin <valentin.grenier02@gmail.com>
2023-10-08 12:04:28 +0200

    Contact page: changed blocks order

--- src/app/mentions-legales/page.tsx
+++ src/app/mentions-legales/page.tsx
@@ -2,15 +2,21 @@ import { axiosInstance } from "@/axios/axios";
 import Section from "@/components/Section/Section";
 
 async function getData() {
-  const res = await axiosInstance.get("http://localhost:1337/api/legal-notice");
+  try {
+    const res = await axiosInstance.get(
+      "http://localhost:1337/api/legal-notice"
+    );
 
-  if (!res) {
-    throw new Error("Failed fetching data");
+    if (!res) {
+      throw new Error("Failed fetching data");
+    }
+
+    const data = await res.data;
+
+    return data.data;
+  } catch (error) {
+    return error;
   }
-
-  const data = await res.data;
-
-  return data.data;
 }
 
 export default async function LegalNotice() {
