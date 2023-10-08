653f6b776c75d09f061b736e591f80bcbe273949
Valentin <valentin.grenier02@gmail.com>
2023-10-08 18:58:22 +0200

    Edited next config to implement images from another domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { hostname } = require("os");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "portfolio-back.local",
+        port: "",
+        pathname: "/wp-content/uploads/**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
