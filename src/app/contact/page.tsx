1f9e8f0f55d785159182d24b18b08a3f6a13d8e3
Valentin <valentin.grenier02@gmail.com>
2023-10-08 09:37:56 +0200

    Contact page: responsive

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,6 +8,7 @@ import ButtonSubmit from "@/components/Button/ButtonSubmit";
 import ButtonLinkedIn from "@/components/Button/ButtonLinkedIn";
 import ButtonGithub from "@/components/Button/ButtonGithub";
 import ButtonMalt from "@/components/Button/ButtonMalt";
+import ButtonContainer from "@/components/Button/ButtonContainer";
 
 export default function Contact() {
   return (
@@ -16,7 +17,7 @@ export default function Contact() {
       <Section>
         <h1>Contact</h1>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
-          <div>
+          <div className="order-last">
             <p className="mb-4">
               Besoin d'échanger avec moi ? C'est avec plaisir que je répondrai à
               votre demande de contact !
@@ -24,14 +25,14 @@ export default function Contact() {
             <p className="mb-8">
               Vous pouvez aussi me trouver sur les réseaux sociaux.
             </p>
-            <div className="flex flex-col gap-2">
+            <ButtonContainer direction="col" position="center">
               <ButtonLinkedIn text={"Mon profil LinkedIn"} />
               <ButtonGithub
                 slug={"https://github.com/valentin-grenier"}
                 text={"Mon GitHub"}
               />
               <ButtonMalt text={"Mon profil Malt"} />
-            </div>
+            </ButtonContainer>
           </div>
           <div>
             <Form>
