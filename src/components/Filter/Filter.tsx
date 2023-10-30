319e3e94f0e37f4dd67c2572b223225f1b6df5ce
valentin-grenier <valentin.grenier02@gmail.com>
2023-10-30 10:02:13 +0100

    Filter component: fixed error on build due to missing dependency in dependency array

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -45,8 +45,7 @@ export default function Filter() {
 
   useEffect(() => {
     handleStackSelect();
-    console.log();
-  }, [selectedStack]);
+  }, [selectedStack, handleStackSelect]);
 
   function handleStackSelect() {
     if (selectedStack === "") {
