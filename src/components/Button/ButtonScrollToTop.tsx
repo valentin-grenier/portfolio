e6f4c9b75e86435467be879116a8ac5f384484db
valentin-grenier <valentin.grenier02@gmail.com>
2023-10-30 09:09:49 +0100

    ButtonScrollToTop: edited button size

--- src/components/Button/ButtonScrollToTop.tsx
+++ src/components/Button/ButtonScrollToTop.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react';
+import React from "react";
 import KeyboardArrowUpRoundedIcon from "@mui/icons-material/KeyboardArrowUpRounded";
 import { useEffect, useState } from "react";
 
@@ -31,12 +31,16 @@ export default function ButtonScrollToTop() {
 
   return (
     <button
-      className={`fixed right-4 bg-white px-1 rounded-lg transition-position duration-500 ${
+      className={`fixed right-4 lg:right-6 bg-white px-1 rounded-lg transition-position duration-500 ${
         isVisible ? "bottom-4" : "-bottom-8"
       }`}
       onClick={scrollToTop}
     >
-      <KeyboardArrowUpRoundedIcon />
+      <KeyboardArrowUpRoundedIcon
+        style={{
+          fontSize: window.innerWidth <= 768 ? "32px" : "40px",
+        }}
+      />
     </button>
   );
 }
