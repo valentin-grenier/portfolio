9fa989d8c848cc1c71d50ef205161484060cda6f
Valentin Grenier <valentin.grenier02@gmail.com>
2024-06-05 12:07:44 +0200

    Removed contact form until I fix it

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,50 +1,51 @@
-import React from "react";
-import Header from "@/components/Header/Header";
-import Footer from "@/components/Footer/Footer";
-import Section from "@/components/Section/Section";
-import Form from "@/components/Form/Form";
-import Input from "@/components/Form/Input";
-import Textarea from "@/components/Form/Textarea";
-import ButtonSubmit from "@/components/Button/ButtonSubmit";
-import ButtonLinkedIn from "@/components/Button/ButtonLinkedIn";
-import ButtonGithub from "@/components/Button/ButtonGithub";
-import ButtonMalt from "@/components/Button/ButtonMalt";
-import ButtonContainer from "@/components/Button/ButtonContainer";
-import { Metadata } from "next";
+import React from 'react';
+import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
+import Section from '@/components/Section/Section';
+import Form from '@/components/Form/Form';
+import Input from '@/components/Form/Input';
+import Textarea from '@/components/Form/Textarea';
+import ButtonSubmit from '@/components/Button/ButtonSubmit';
+import ButtonLinkedIn from '@/components/Button/ButtonLinkedIn';
+import ButtonGithub from '@/components/Button/ButtonGithub';
+import ButtonMalt from '@/components/Button/ButtonMalt';
+import ButtonContainer from '@/components/Button/ButtonContainer';
+import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "Contact · Valentin Grenier, Développeur Web Front-End",
-  description:
-    "Prêt à collaborer sur un projet ? Contactez-moi pour discuter de vos idées. Je suis impatient de travailler ensemble pour réaliser votre projet.",
+	title: 'Contact · Valentin Grenier, Développeur Web Front-End',
+	description:
+		'Prêt à collaborer sur un projet ? Contactez-moi pour discuter de vos idées. Je suis impatient de travailler ensemble pour réaliser votre projet.',
 };
 
 export default function Contact() {
-  return (
-    <>
-      <Section>
-        <h1>Contact</h1>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
-          <div className="order-last lg:order-first">
-            <p className="mb-4">
-              Besoin d'échanger avec moi ? C'est avec plaisir que je répondrai à
-              votre demande de contact !
-            </p>
-            <p className="mb-8">
-              Vous pouvez aussi me trouver sur les réseaux sociaux.
-            </p>
-            <ButtonContainer direction="col" position="center">
-              <ButtonLinkedIn text={"Mon profil LinkedIn"} />
-              <ButtonGithub
-                slug={"https://github.com/valentin-grenier"}
-                text={"Mon GitHub"}
-              />
-              <ButtonMalt text={"Mon profil Malt"} />
-            </ButtonContainer>
-          </div>
+	return (
+		<>
+			<Section>
+				<h1>Contact</h1>
+				<div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
+					<div className="order-last lg:order-first">
+						<p className="mb-4">
+							Besoin d'échanger avec moi ? C'est avec plaisir que
+							je répondrai à votre demande de contact !
+						</p>
+						<p className="mb-8">
+							Vous pouvez aussi me trouver sur les réseaux
+							sociaux.
+						</p>
+						<ButtonContainer direction="col" position="center">
+							<ButtonLinkedIn text={'Mon profil LinkedIn'} />
+							<ButtonGithub
+								slug={'https://github.com/valentin-grenier'}
+								text={'Mon GitHub'}
+							/>
+							<ButtonMalt text={'Mon profil Malt'} />
+						</ButtonContainer>
+					</div>
 
-          <Form />
-        </div>
-      </Section>
-    </>
-  );
+					{/* <Form /> */}
+				</div>
+			</Section>
+		</>
+	);
 }
