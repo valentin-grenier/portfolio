38b6882941ecff8c986bed9100cbecce86dcbb79
Valentin Grenier <valentin.grenier02@gmail.com>
2025-12-06 08:43:29 +0100

    fix: Footer - Replaced static year by dynamic one with start date

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,9 +5,9 @@ export default function Footer() {
   return (
     <footer className="flex flex-col md:flex-row md:justify-between my-8 px-4">
       <div className="mb-8 md:mb-0">
-        <p className="font-title font-semibold text-center">
-          Créé avec passion à{" "}
-          <strong className="text-danube-accent">Toulouse</strong> - 2023
+        <p className="flex flex-col items-start font-title font-semibold text-center">
+          <span>Créé avec passion à{" "}<strong className="text-danube-accent">Toulouse</strong></span>
+          <span>2018 - {new Date().getFullYear()}</span>
         </p>
       </div>
       <div>
